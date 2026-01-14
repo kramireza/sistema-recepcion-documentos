af565ea676d99eb507501ef355e6d9e15cc6bfcf
kramireza <kr31kevinramirez9794@outlook.com>
2026-01-13 23:25:26 -0600

    update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "/api",
 });
 
 api.interceptors.request.use(config => {
